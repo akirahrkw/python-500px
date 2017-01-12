c1b89230309d81b3750161a69f66af8ce3172d08
akirahrkw <akirahrkw@gmail.com>
2017-01-12 18:28:46 +0800

    version 0.3

--- fivehundredpx/__init__.py
+++ fivehundredpx/__init__.py
@@ -3,9 +3,9 @@ from fivehundredpx.auth   import *
 from fivehundredpx.errors import *
 
 """
-500px API library 
+500px API library
 inspired by tweepy(https://github.com/tweepy/tweepy), python-instagram(https://github.com/Instagram/python-instagram)
 """
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 __author__  = 'Akira Hirakawa'
 __license__ = 'MIT'
